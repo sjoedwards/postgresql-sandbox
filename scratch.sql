bb77a7e3ab1a0b8bd035134144dd05f7f7e765d6
sjoedwards <samuel.j.o.edwards@gmail.com>
2021-09-03 10:32:07 +0100

    43

--- scratch.sql
+++ scratch.sql
@@ -1,4 +1,16 @@
-SELECT
-  CONCAT(UPPER(name), ', ', UPPER(country)) AS location
-FROM
-  cities;
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(200),
+  user_id INTEGER REFERENCES users(id)
+);
+
+INSERT INTO
+  photos (url, user_id)
+VALUES
+  ('http:/one.jpg', 4),
+  ('http:/two.jpg', 1),
+  ('http:/25.jpg', 1),
+  ('http:/36.jpg', 1),
+  ('http:/754.jpg', 2),
+  ('http:/35.jpg', 3),
+  ('http:/256.jpg', 4);
